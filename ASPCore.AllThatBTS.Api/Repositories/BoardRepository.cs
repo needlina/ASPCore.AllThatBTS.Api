0e2415842cfc3aec33c85b651010d0bc50c90188
Lee Hyun jin <needlina@hotmail.com>
2019-02-22 17:50:55 +0900

    BoardRepository, BoardService Draft

--- ASPCore.AllThatBTS.Api/Repositories/BoardRepository.cs
+++ ASPCore.AllThatBTS.Api/Repositories/BoardRepository.cs
@@ -1,12 +1,275 @@
 ﻿using ASPCore.AllThatBTS.Api.Common;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ASPCore.AllThatBTS.Api.Repositories
 {
     public class BoardRepository : BaseRepository
     {
+        #region 게시판
+        /// <summary>
+        /// 게시판 조회 페이지 컨트롤
+        /// </summary>
+        /// <param name="boardId">게시판 ID</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public BoardPageInfoT SelectBoardPageInfo(string boardId, int pageNo)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                BOARD_ID = boardId,
+                PAGE_NO = pageNo
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+
+        /// <summary>
+        /// 게시판의 카테고리 리스트 조회
+        /// </summary>
+        /// <param name="boardId">게시판 ID</param>
+        /// <returns></returns>
+        public List<BoardCategoryT> SelectBoardCategoryList(string boardId)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                BOARD_ID = boardId
+            };
+
+            return Connection.Fetch<BoardCategoryT>(sql, parameters);
+        }
+        #endregion
+
+        #region 게시물
+        /// <summary>
+        /// 게시물 리스트 조회
+        /// </summary>
+        /// <param name="boardId">게시판ID</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <param name="searchType">검색 조건 타입 : 제목,내용,작성자</param>
+        /// <param name="searchKeyword">검색어</param>
+        /// <returns></returns>
+        public List<ArticleT> SelectBoardList(string boardId,
+                                              int pageSize,
+                                              int pageNo,
+                                              string searchType,
+                                              string searchKeyword)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                BOARD_ID = boardId,
+                PAGE_SIZE = pageSize,
+                PAGE_NO = pageNo,
+                SEARCH_TYPE = searchType,
+                SEARCH_KEYWORD = searchKeyword
+            };
+
+            return Connection.Fetch<ArticleT>(sql, parameters);
+        }
+
+        /// <summary>
+        /// 게시물 조회 (Detail)
+        /// </summary>
+        /// <param name="seq">Sequence 값</param>
+        /// <returns></returns>
+        public ArticleT SelectArticle(string seq)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SEQ = seq
+            };
+
+            return Connection.SingleOrDefault<ArticleT>(sql, parameters);
+        }
+
+
+        /// <summary>
+        /// 게시물 작성
+        /// </summary>
+        /// <param name="article">게시물 모델</param>
+        /// <returns></returns>
+        public int InsertArticle(ArticleT article)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                BOARD_ID = boardId,
+                PAGE_SIZE = pageSize,
+                PAGE_NO = pageNo,
+                SEARCH_TYPE = searchType,
+                SEARCH_KEYWORD = searchKeyword
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+        
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <param name="article">게시물 모델</param>
+        /// <returns></returns>
+        public int UpdateArticle(ArticleT article)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                BOARD_ID = boardId,
+                PAGE_SIZE = pageSize,
+                PAGE_NO = pageNo,
+                SEARCH_TYPE = searchType,
+                SEARCH_KEYWORD = searchKeyword
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <param name="seq">게시물 Seq</param>
+        /// <returns></returns>
+        public int DeleteArticle(string seq)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SEQ = seq
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+        #endregion
+
+        #region 댓글
+        /// <summary>
+        /// 댓글 리스트 조회
+        /// </summary>
+        /// <param name="seq">게시물 Sequence</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public List<CommentT> SelectCommentList(string seq, 
+                                                int pageSize,
+                                                int pageNo)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SEQ = seq,
+                PAGE_SIZE = pageSize,
+                PAGE_NO = pageNo
+            };
+
+            return Connection.Fetch<CommentT>(sql, parameters);
+        }
+
+
+        /// <summary>
+        /// 댓글 추가
+        /// </summary>
+        /// <param name="comment">댓글 모델</param>
+        /// <returns></returns>
+        public CommentT InsertComment(CommentT comment)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                // 댓글 모델
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+
+        /// <summary>
+        /// 댓글 삭제 (DeleteFlag)
+        /// </summary>
+        /// <param name="commentSeq">댓글 Sequence</param>
+        /// <returns></returns>
+        public int DeleteComment(string commentSeq)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SEQ = commentSeq
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+
+        /// <summary>
+        /// 대댓글 리스트 조회
+        /// </summary>
+        /// <param name="seq">게시물 Sequence</param>
+        /// <param name="commentSeq">댓글 Sequence</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public List<CommentT> SelectSubCommentList(string seq,
+                                                   string commentSeq,
+                                                   int pageSize,
+                                                   int pageNo)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SEQ = seq,
+                COMMENT_SEQ = commentSeq,
+                PAGE_SIZE = pageSize,
+                PAGE_NO = pageNo
+            };
+
+            return Connection.Fetch<CommentT>(sql, parameters);
+        }
+
+        /// <summary>
+        /// 대댓글 추가
+        /// </summary>
+        /// <param name="subComment">대댓글 모델</param>
+        /// <returns></returns>
+        public int InsertSubComment(CommentT subComment)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                // 대댓글 모델
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+
+        /// <summary>
+        /// 대댓글 삭제
+        /// </summary>
+        /// <param name="subCommentSeq">대댓글 Sequence</param>
+        /// <returns></returns>
+        public int DeleteSubComment(string subCommentSeq)
+        {
+            string sql = SQLHelper.GetSqlByMethodName(MethodBase.GetCurrentMethod().Name);
+
+            var parameters = new
+            {
+                SUBCOMMENT_SEQ = subCommentSeq
+            };
+
+            return Connection.Execute(sql, parameters);
+        }
+        #endregion
     }
 }
--- ASPCore.AllThatBTS.Api/Services/BoardService.cs
+++ ASPCore.AllThatBTS.Api/Services/BoardService.cs
@@ -1,12 +1,172 @@
-﻿using System;
+﻿using ASPCore.AllThatBTS.Api.Repositories;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASPCore.AllThatBTS.Api.Services
 {
     public class BoardService
     {
+        private readonly BoardRepository boardRepository;
 
+        public BoardService()
+        {
+            boardRepository = new BoardRepository();
+        }
+
+        #region 게시판
+        /// <summary>
+        /// 게시판 조회 페이지 컨트롤
+        /// </summary>
+        /// <param name="boardId">게시판 ID</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public BoardPageInfoT GetBoardPageInfo(string boardId, int pageNo)
+        {
+            return boardRepository.SelectBoardPageInfo(boardId, pageNo);
+        }
+
+        /// <summary>
+        /// 게시판의 카테고리 리스트 조회
+        /// </summary>
+        /// <param name="boardId">게시판 ID</param>
+        /// <returns></returns>
+        public List<BoardCategoryT> GetBoardCategoryList(string boardId)
+        {
+            return boardRepository.SelectBoardCategoryList(boardId);
+        }
+        #endregion
+
+        #region 게시물
+        /// <summary>
+        /// 게시물 리스트 조회
+        /// </summary>
+        /// <param name="boardId">게시판ID</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <param name="searchType">검색 조건 타입 : 제목,내용,작성자</param>
+        /// <param name="searchKeyword">검색어</param>
+        /// <returns></returns>
+        public List<ArticleT> GetBoardList(string boardId,
+                                              int pageSize,
+                                              int pageNo,
+                                              string searchType,
+                                              string searchKeyword)
+        {
+            return boardRepository.SelectBoardList(boardId, pageSize, pageNo, searchType, searchKeyword);
+        }
+
+        /// <summary>
+        /// 게시물 조회 (Detail)
+        /// </summary>
+        /// <param name="seq">Sequence 값</param>
+        /// <returns></returns>
+        public ArticleT GetArticle(string seq)
+        {
+            return boardRepository.SelectArticle(seq);
+        }
+
+
+        /// <summary>
+        /// 게시물 작성
+        /// </summary>
+        /// <param name="article">게시물 모델</param>
+        /// <returns></returns>
+        public int CreateArticle(ArticleT article)
+        {
+            return boardRepository.InsertArticle(article);
+        }
+
+        /// <summary>
+        /// 게시물 수정
+        /// </summary>
+        /// <param name="article">게시물 모델</param>
+        /// <returns></returns>
+        public int SetArticle(ArticleT article)
+        {
+            return boardRepository.UpdateArticle(article);
+        }
+
+        /// <summary>
+        /// 게시물 삭제
+        /// </summary>
+        /// <param name="seq">게시물 Seq</param>
+        /// <returns></returns>
+        public int RemoveArticle(string seq)
+        {
+            return boardRepository.DeleteArticle(seq);
+        }
+        #endregion
+
+        #region 댓글
+        /// <summary>
+        /// 댓글 리스트 조회
+        /// </summary>
+        /// <param name="seq">게시물 Sequence</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public List<CommentT> GetCommentList(string seq,
+                                                int pageSize,
+                                                int pageNo)
+        {
+            return boardRepository.SelectCommentList(seq, pageSize, pageNo);
+        }
+
+
+        /// <summary>
+        /// 댓글 추가
+        /// </summary>
+        /// <param name="comment">댓글 모델</param>
+        /// <returns></returns>
+        public CommentT CreateComment(CommentT comment)
+        {
+            return boardRepository.InsertComment(comment);
+        }
+
+        /// <summary>
+        /// 댓글 삭제 (DeleteFlag)
+        /// </summary>
+        /// <param name="commentSeq">댓글 Sequence</param>
+        /// <returns></returns>
+        public int RemoveComment(string commentSeq)
+        {
+            return boardRepository.DeleteComment(commentSeq);
+        }
+
+        /// <summary>
+        /// 대댓글 리스트 조회
+        /// </summary>
+        /// <param name="seq">게시물 Sequence</param>
+        /// <param name="commentSeq">댓글 Sequence</param>
+        /// <param name="pageSize">페이지 사이즈</param>
+        /// <param name="pageNo">페이지 번호</param>
+        /// <returns></returns>
+        public List<CommentT> GetSubCommentList(string seq,
+                                                   string commentSeq,
+                                                   int pageSize,
+                                                   int pageNo)
+        {
+            return boardRepository.SelectSubCommentList(seq, commentSeq, pageSize, pageNo);
+        }
+
+        /// <summary>
+        /// 대댓글 추가
+        /// </summary>
+        /// <param name="subComment">대댓글 모델</param>
+        /// <returns></returns>
+        public int CreateSubComment(CommentT subComment)
+        {
+            return boardRepository.InsertSubComment(subComment);
+        }
+
+        /// <summary>
+        /// 대댓글 삭제
+        /// </summary>
+        /// <param name="subCommentSeq">대댓글 Sequence</param>
+        /// <returns></returns>
+        public int RemoveSubComment(string subCommentSeq)
+        {
+            return boardRepository.DeleteSubComment(subCommentSeq);
+        }
+        #endregion
     }
 }
